3b62946a3a8624777340c8f2375ff9d30d7f0834
paulizleet <paulitogallagher@gmail.com>
2018-02-10 11:53:35 -0500

    Implemented post pruner, finalized post retriever

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,52 +10,79 @@ class PostsController < ApplicationController
   end
 
   def show
-      @posts = Post.where(text_hash: params[:id])
+      @posts = Post.where(post_num: params[:id])
   end
 
 
   def new_threads
     get_threads("pol")
-    render :index
+    redirect_to '/'
+  end
+
+  def purge
+    purge_old_posts("pol")
   end
 
   private
+  def purge_old_posts(board)
+    tracked_threads = {}
+    Posts.select(:op).distinct.where(board: board).each{|e| tracked_threads.merge!({e.op => false})}
+    current_threads = Fourchan::Kit::Board(board).all_threads
+    current_threads.each{ |t| tracked_threads[t.no] = true }
+    tracked_threads.each_key do |k|
+      next if tracked_threads[k]
+      old_posts = Posts.where(op: tracked_threads[k])
+      old_posts.each do |o|
+        old_counter = PostCounter.find_by(text_hash: o.text_hash).destroy
+        o.destroy
+      end
+    end
+  end
+
   def get_threads(board)
     b = Fourchan::Kit::Board.new board
     puts "got board #{board}"
+    threads = b.all_threads
     posts = b.all_posts
     puts "got posts"
+    threads.each do |t|
 
-    posts.each do |r|
-
-      #p r.com
-      @post = Post.find_by(post_num: r.no)
-      cleaned = clean_post(r.com)
-      next if cleaned.nil?
-      if @post.nil?
-          @post = Post.new(
-            text_hash: XXhash.xxh32(cleaned),
+      t.posts.each do |r|
+        next if Post.exists?(post_num: r.no) && any_text?(r.com)
+        @post = Post.new(
+            text_hash: XXhash.xxh32(r.com),
             board: board,
             op: r.resto,
             post_num: r.no,
             poster_id: r.id,
-            text: cleaned,
+            text: r.com,
             post_timestamp: r.time
           )
-          @post.save
-        else
-          p "not saving"
+        @post.save
+        @post.increment
       end
-      @post.increment
-
-
-
     end
   end
 
-  def clean_post(text)
-    #remove html formatting and shit
-    text
+  def any_text?(text)
+    return false if text.nil?
+    #test to see if it's just a (you)
+    stripped = text.strip
+    stack = []
+    i=0
+    while i < stripped.length
+      i+=1
+      case stripped[i]
+      when "<"
+        stack.push(true)
+      when ">"
+        stack.pop
+      else
+        nil
+      end
+      return true if i > stripped.length
+    end
+    false
   end
 
   def get_post
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,6 @@ class Post < ApplicationRecord
   belongs_to :post_counter, required: false
   def increment
     @counter = PostCounter.find_by(text_hash: self.text_hash)
-
     if @counter == nil
       PostCounter.new(text_hash: self.text_hash, occurrences: 1).save
     else

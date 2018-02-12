2f3745f3c36a7512fa4f383748f83c9d7bf2cf59
paulizleet <paulitogallagher@gmail.com>
2018-02-12 14:00:21 -0500

    Created worker classes for queueing jobs

---
+++ app/workers/post_fetching_worker.rb
@@ -0,0 +1,80 @@
+class PostFetchingWorker
+  def enqueue
+    puts "--------------"
+    puts "Queued post fetcher"
+    puts "--------------"
+  end
+  def before
+    puts "--------------"
+    puts "beginning post fetcher"
+    puts "--------------"
+  end
+
+  def success
+    puts "--------------"
+    puts "Post fetcher finished succesfully"
+    puts "--------------"
+  end
+
+  def failure
+    puts "--------------"
+    puts "post fetcher failed"
+    puts "--------------"
+  end
+
+  #gets all currently active threads and catalogs the posts
+  def perform_without_delay(board)
+    puts "Fetching Posts"
+    b = Fourchan::Kit::Board.new board
+    threads = b.threads(1)
+    threads.each do |t|
+
+      begin
+        reps = Fourchan::Kit::Thread.new(board, t.no).fetch_replies
+      rescue
+        next
+      end
+      #The OP is not included when fetching replies - so add it to the list
+      reps.insert(0, t)
+      reps.each do |r|
+        next if Post.exists?(post_num: r.no) && any_text?(r.com)
+        @post = Post.new(
+            text_hash: XXhash.xxh32(r.com),
+            board: board,
+            op: r.resto != 0 ? r.resto : r.no,
+            post_num: r.no,
+            poster_id: r.id,
+            text: r.com,
+            post_timestamp: r.time
+          )
+        @post.save
+        @post.increment
+      end
+    end
+  end
+  handle_asynchronously :perform_without_delay
+
+
+  private
+  def any_text?(text)
+    return false if text.nil?
+    #test to see if it's just a (you)
+    stripped = text.strip
+    stack = []
+    i=0
+    while i < stripped.length
+      i+=1
+      case stripped[i]
+      when "<"
+        stack.push(true)
+      when ">"
+        stack.pop
+      else
+        nil
+      end
+      return true if i > stripped.length
+    end
+    false
+  end
+
+end
---
+++ app/workers/thread_pruning_worker.rb
@@ -0,0 +1,41 @@
+class ThreadPruningWorker
+
+
+  def enqueue
+    puts "--------------"
+    puts "Queued thread pruner"
+    puts "--------------"
+  end
+
+  def success
+    puts "--------------"
+    puts "Thread Pruner finished succesfully"
+    puts "--------------"
+  end
+
+  def failure
+    puts "--------------"
+    puts "Thread Pruner failed"
+    puts "--------------"
+  end
+
+  #removes threads and posts which are no longer active
+  def pperform(board)
+    tracked_threads = {}
+    Post.select(:op).distinct.where(board: board).each{|e| tracked_threads.merge!({e.op => false})}
+    current_threads = Fourchan::Kit::Board.new(board).all_threads
+    current_threads.each{ |t| tracked_threads[t.no.to_s] = true }
+    tracked_threads.each_key do |k|
+      #binding.pry
+      next if tracked_threads[k] == true
+      old_posts = Post.where(op: k)
+      old_posts.each do |o|
+        old_counter = PostCounter.find_by(text_hash: o.text_hash)
+        old_counter.destroy unless old_counter.nil?
+      end
+      old_posts.destroy_all
+    end
+  end
+  handle_asynchronously :purge_old_posts
+
+end

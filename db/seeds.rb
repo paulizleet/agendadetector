2731021b2d3a0dc85f43c9ff95527619d6c73d0c
Paul Gallagher <paulitogalalgher@gmail.com>
2018-09-12 14:29:53 -0400

    Seeds all current boards and troll flags on 4chan

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-ChanBoard.new(board_id: 'his', board_name: 'History & Humanities').save
-ChanBoard.new(board_id: 'pol', board_name: 'Politically Incorrect').save
-# Board.new(board_id: 'vg', board_name: 'Video Game Generals').save
-ChanBoard.new(board_id: 'v', board_name: 'Video Games').save
-ChanBoard.new(board_id: 'biz', board_name: 'Business & Finance').save
-ChanBoard.new(board_id: 'int', board_name: 'International').save
+#Get boards the old fashioned way without fancy gems
+
+url = URI.parse('http://a.4cdn.org/boards.json')
+request = Net::HTTP::Get.new(url.to_s)
+response = Net::HTTP.start(url.host, url.port) {|http|
+  http.request(request)
+}
+
+json = JSON.parse(response.body)
+
+json["boards"].each do |board|
+    ChanBoard.new(board_id: board["board"], board_name: board["title"]).save
+end
+
+json["troll_flags"].each_pair {|id,title| NatFlag.new(flag_id: id, flag_name: title, troll:true)}
+
+# ChanBoard.new(board_id: 'his', board_name: 'History & Humanities').save
+# ChanBoard.new(board_id: 'pol', board_name: 'Politically Incorrect').save
+# ChanBoard.new(board_id: 'vg', board_name: 'Video Game Generals').save
+# ChanBoard.new(board_id: 'v', board_name: 'Video Games').save
+# ChanBoard.new(board_id: 'biz', board_name: 'Business & Finance').save
+# ChanBoard.new(board_id: 'int', board_name: 'International').save
 
 
 #add meme table so they aren't displayed in top posts

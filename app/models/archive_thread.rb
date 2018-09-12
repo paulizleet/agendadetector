74bedaa2eb39daf340f46ef7b7f39e0ebcb043dd
Paul Gallagher <paulitogalalgher@gmail.com>
2018-09-12 14:27:21 -0400

    No longer archives empty/reply only posts

--- app/models/archive_thread.rb
+++ app/models/archive_thread.rb
@@ -2,6 +2,7 @@ class ArchiveThread < ApplicationRecord
   belongs_to :chan_board
   has_many :archive_posts
   def new_post(post)
+    return if post.text_hash == "46947589" #Hash of an empty string.  Do not archive these.
     @post = self.archive_posts.new(
         chan_board_id: post.chan_board_id,
         text_hash: post.text_hash,
